82f7954ac5ec3f2b063a5635b9b11388c8dd2d43
Index <131189883+1ndex13@users.noreply.github.com>
2025-03-26 09:03:27 +0300

    Delete src/components/api directory

--- src/components/api/axiosConfig.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080', // Убедитесь, что URL совпадает с вашим бэкендом
-  withCredentials: true, // Включает отправку cookie
-});
-
-export default axiosInstance;
